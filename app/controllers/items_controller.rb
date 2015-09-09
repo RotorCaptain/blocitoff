1686c12312bb3f49c61867b8f70d7d3431c4f860
Chris Steeb <chris.steeb.001@gmail.com>
2015-09-09 20:24:58 +0000

    Assignment 5 complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,4 +32,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :body)
   end
-ends
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,39 @@
+require 'faker'
+
+#Create Users
+10.times do
+  user = User.new(
+          name:         Faker::Name.name,
+          email:        Faker::Internet.email,
+          password:     Faker::Lorem.characters(10) 
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+user = User.first
+user.skip_confirmation!
+user.update_attributes!(
+  email: 'rotorcaptain@hotmail.com',
+  password: 'cyclic01',
+  name: 'Chris'
+  )
+
+50.times do
+  Item.create!(
+              user:         users.sample,
+              name:         Faker::Lorem.sentence,
+              created_at:   Faker::Date.backward(7)
+
+  )
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
